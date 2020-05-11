9d6a35c7979e58fff462ba006a21f2e650e90386
qieerxi <1574408801@qq.com>
2020-05-11 09:23:08 +0800

    li

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Homepage from '@/components/Homepage'
-import Citylist from '@/components/Citylist'
+import Homepage from '@/components/Homepage';
+import Citylist from '@/components/Citylist';
 
 Vue.use(Router);
 
